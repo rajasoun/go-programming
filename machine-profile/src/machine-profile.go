8b0f8166b60ed77e8bd7ce8e2180e67d58153c3f
Raja Soundaramourty <rajasoun@icloud.com>
2022-01-11 09:39:14 +0000

    refactor code

--- machine-profile/src/machine-profile.go
+++ machine-profile/src/machine-profile.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 )
 
+func printRuntimeProfile() {
+	fmt.Printf("Running %s go on %s\n", os.Args[0], os.Getenv("GOFILE"))
+}
+
+func printCurrentWorkingDir() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("  cwd = %s\n", cwd)
+}
+
+func printCmdLineArgs() {
+	fmt.Printf("  os.Args = %#v\n", os.Args)
+}
+
+func printRuntimeDetails() {
+	fmt.Println("  GOARCH = ", runtime.GOARCH)
+	fmt.Println("  GOOS = ", runtime.GOOS)
+	fmt.Println("  CPU Cores = ", runtime.NumCPU())
+	fmt.Println("  Go Vesrion = ", runtime.Version())
+}
+
 func main() {
-	fmt.Println("      GOOS : ", runtime.GOOS)
-	fmt.Println("    GOARCH : ", runtime.GOARCH)
-	fmt.Println(" CPU Cores : ", runtime.NumCPU())
-	fmt.Println("Go Vesrion : ", runtime.Version())
+	printRuntimeProfile()
+	printCurrentWorkingDir()
+	printCmdLineArgs()
+	printRuntimeDetails()
 }
