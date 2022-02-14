497ec5899a9ff16b6ef425f47abcd03db1d09a0c
Raja Soundaramourty <rajasoun@icloud.com>
2022-02-14 03:39:19 +0000

    fix template go module creation

--- coursera/gen-module.sh
+++ coursera/gen-module.sh
@@ -4,11 +4,11 @@ read -p 'Module Name: ' module
 
 echo -e "Creating $module"
 mkdir -p "$module"/{bin,pkg,src}
-cp hello-world/.gitignore           "$module/"
-cp hello-world/Makefile             "$module/"
-cp hello-world/go.mod               "$module/"
-cp hello-world/src/hello.go         "$module/src/$module.go"
-sed  -i "s/hello-world/$module/g"   "$module/go.mod"
+cp hello/.gitignore           "$module/"
+cp hello/Makefile             "$module/"
+cp hello/go.mod               "$module/"
+cp hello/src/hello.go         "$module/src/$module.go"
+sed  -i "s/hello/$module/g"   "$module/go.mod"
 sed  -i "s/Hello, world!/$module/g" "$module/src/$module.go"
 
 
